848cb55ee72b6acfb5fff24fb15bb20c7e1c4186
Zazacito <victor.azalbert@yahoo.fr>
2023-11-09 09:45:33 +0100

    Main Update

--- functions/index.js
+++ functions/index.js
@@ -121,6 +121,90 @@ app.post("/catapult/sessionrawdata", async (req, res) => {
   }
 });
 
+app.post("/catapult/sessioneffortsdata", async (req, res) => {
+  try {
+    const { sessionId, athleteId, apiKey } = req.body;
+    sdk.auth(apiKey);
+    sdk.server("https://connect-eu.catapultsports.com/api/v6");
+
+    const { data } = await sdk.getEffortsDataForAthleteInActivity({
+      effort_types: 'acceleration,velocity',
+      velocity_bands: '6,7,8',
+      acceleration_bands: '-3,3',
+      activity_id: sessionId,
+      athlete_id: athleteId,
+    })
+
+    if (data.length > 0) {
+      res.json(data[0].data);
+    } else {
+      res.status(404).json({ error: "No session data found for the session ID and athlete ID." });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "An error occurred while fetching the session efforts data.", error: error });
+  }
+});
+
+app.post("/catapult/sessioneventsdata", async (req, res) => {
+  try {
+    const { sessionId, athleteId, apiKey } = req.body;
+    sdk.auth(apiKey);
+    sdk.server("https://connect-eu.catapultsports.com/api/v6");
+
+    const { data } = await sdk.getEventsDataForAthleteInActivity({
+      event_types: 'rugby_union_contact_involvement,rugby_league_tackle,rugby_union_kick',
+      activity_id: sessionId,
+      athlete_id: athleteId,
+    })
+
+    if (data.length > 0) {
+      res.json(data[0].data);
+    } else {
+      res.status(404).json({ error: "No session data found for the session ID and athlete ID." });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "An error occurred while fetching the session events data.", error: error });
+  }
+});
+
+
+app.post("/catapult/sessionannotations", async (req, res) => {
+  try {
+    const { sessionId, apiKey } = req.body;
+    sdk.auth(apiKey);
+    sdk.server("https://connect-eu.catapultsports.com/api/v6");
+
+    const { data } = await sdk.getActivityAnnotations({ id: sessionId })
+
+    if (data.length > 0) {
+
+      res.json(data);
+    } else {
+      res.status(404).json({ error: "No annotation data found for the session ID." });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "An error occurred while fetching the session annotation data.", error: error });
+  }
+});
+
+app.post("/catapult/sessionperiods", async (req, res) => {
+  try {
+    const { sessionId, apiKey } = req.body;
+    sdk.auth(apiKey);
+    sdk.server("https://connect-eu.catapultsports.com/api/v6");
+
+    const { data } = await sdk.getPeriodsInActivity({ id: sessionId })
+
+    if (data.length > 0) {
+      res.json(data);
+    } else {
+      res.status(404).json({ error: "No periods data found for the session ID." });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "An error occurred while fetching the session periods data.", error: error });
+  }
+});
+
 
 
 exports.api = functions.runWith({ timeoutSeconds: 260 }).https.onRequest(app);
